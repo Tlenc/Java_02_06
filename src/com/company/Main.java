ab2b66e5a151b51971ab95b5eaa71d787a82ade5
Tlenc <tlenciauskas@gmail.com>
2018-01-30 15:44:45 +0200

    Code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	System.out.println("Įveskite savo svorį ir ūgį");
+        Scanner sc = new Scanner(System.in);
+        float kg = sc.nextFloat();
+        float m = sc.nextFloat();
+        float kmi = kg/(m*m);
+        System.out.println("KMI :" + kmi);
     }
 }
